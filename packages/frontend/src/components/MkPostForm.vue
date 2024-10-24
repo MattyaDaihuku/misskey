03ac79c8d56e273b22410ae0384452c9bf2634d0
MattyaDaihuku <87429435+MattyaDaihuku@users.noreply.github.com>
2024-10-24 12:28:16 +0000

    enhance(frontend): リモートユーザーへのリプライは強制的に連合させるように

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -32,7 +32,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 					<span :class="$style.headerRightButtonText">{{ channel.name }}</span>
 				</button>
 			</template>
-			<button v-click-anime v-tooltip="i18n.ts._visibility.disableFederation" class="_button" :class="[$style.headerRightItem, { [$style.danger]: localOnly }]" :disabled="channel != null || visibility === 'specified'" @click="toggleLocalOnly">
+			<button v-click-anime v-tooltip="i18n.ts._visibility.disableFederation" class="_button" :class="[$style.headerRightItem, { [$style.danger]: localOnly }]" :disabled="channel != null || visibility === 'specified' || remoteReply" @click="toggleLocalOnly">
 				<span v-if="!localOnly"><i class="ti ti-rocket"></i></span>
 				<span v-else><i class="ti ti-rocket-off"></i></span>
 			</button>
@@ -201,6 +201,19 @@ const recentHashtags = ref(JSON.parse(miLocalStorage.getItem('hashtags') ?? '[]'
 const imeText = ref('');
 const showingOptions = ref(false);
 const textAreaReadOnly = ref(false);
+const isLocalUser = (user) => {
+	return user.host == null || user.host === host;
+};
+
+const isReplyUserLocal = computed(() => {
+	return props.reply ? isLocalUser(props.reply.user) : true;
+});
+
+const hasRemoteMentions = computed(() => {
+	return visibleUsers.value.some(user => !isLocalUser(user));
+});
+
+const remoteReply = computed(() => !isReplyUserLocal.value && hasRemoteMentions.value);
 
 const draftKey = computed((): string => {
 	let key = props.channel ? `channel:${props.channel.id}` : '';
