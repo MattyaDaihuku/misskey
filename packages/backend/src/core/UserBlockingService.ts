4bd06bf6f3f31b9a47c600fc8a91cf4729c34f11
lqvp <183242690+lqvp@users.noreply.github.com>
2025-02-11 09:01:46 +0900

    fix(backend): 不要なawaitを削除

--- packages/backend/src/core/UserBlockingService.ts
+++ packages/backend/src/core/UserBlockingService.ts
@@ -100,7 +100,7 @@ export class UserBlockingService implements OnModuleInit {
 
 		// フォロー履歴に「blocked」を保存
 		if (this.userEntityService.isLocalUser(blocker) && blockerPolicies.canReadFollowHistory) {
-			await this.followHistoryRepository.insert({
+			this.followHistoryRepository.insert({
 				id: this.idService.gen(),
 				type: 'blocked',
 				fromUserId: blocker.id,
@@ -111,7 +111,7 @@ export class UserBlockingService implements OnModuleInit {
 
 		// フォロー履歴に「wasBlocked」を保存
 		if (this.userEntityService.isLocalUser(blockee) && blockeePolicies.canReadFollowHistory) {
-			await this.followHistoryRepository.insert({
+			this.followHistoryRepository.insert({
 				id: this.idService.gen(),
 				type: 'wasBlocked',
 				fromUserId: blocker.id,
@@ -217,7 +217,7 @@ export class UserBlockingService implements OnModuleInit {
 
 		// フォロー履歴に「unBlocked」を保存
 		if (this.userEntityService.isLocalUser(blocker) && blockerPolicies.canReadFollowHistory) {
-			await this.followHistoryRepository.insert({
+			this.followHistoryRepository.insert({
 				id: this.idService.gen(),
 				type: 'unBlocked',
 				fromUserId: blocker.id,
@@ -228,7 +228,7 @@ export class UserBlockingService implements OnModuleInit {
 
 		// フォロー履歴に「wasUnBlocked」を保存
 		if (this.userEntityService.isLocalUser(blockee) && blockeePolicies.canReadFollowHistory) {
-			await this.followHistoryRepository.insert({
+			this.followHistoryRepository.insert({
 				id: this.idService.gen(),
 				type: 'wasUnBlocked',
 				fromUserId: blocker.id,
